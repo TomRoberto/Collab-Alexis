5c0be1cc44fd5f796e9b51901417f1f54ba2bc77
Tom ROBERTO <tom.roberto31@gmail.com>
2025-06-13 12:07:01 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header
+      style={{
+        height: "50px",
+        backgroundColor: "red",
+      }}
+    >
+      Je suis un header
+    </header>
+  );
+};
+
+export default Header;
